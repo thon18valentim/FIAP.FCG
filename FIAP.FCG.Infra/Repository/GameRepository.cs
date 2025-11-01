23e45346bb879c68a496584d93469d116d671010
Luiz Fonseca <japalug2005@gmail.com>
2025-11-01 15:03:18 -0300

    Implementação de Dtos (Response, Update e Register seguindo o padrão do código), adição de endpoints na controller do game e adição de funções na gameservice.

--- FIAP.FCG.Infra/Repository/GameRepository.cs
+++ FIAP.FCG.Infra/Repository/GameRepository.cs
@@ -1,13 +1,42 @@
-﻿using FIAP.FCG.Core.Models;
+﻿using AutoMapper;
+using FIAP.FCG.Core.Inputs;
+using FIAP.FCG.Core.Models;
 using FIAP.FCG.Infra.Context;
 
 namespace FIAP.FCG.Infra.Repository
 {
-	public class GameRepository(ApplicationDbContext context) : EFRepository<Game>(context), IGameRepository
+	public class GameRepository(ApplicationDbContext context, IMapper mapper) : EFRepository<Game>(context), IGameRepository
 	{
-		public async Task<IEnumerable<Game>> GetAll()
+		private readonly IMapper _mapper = mapper;
+
+        public async Task<GameRegisterDto> Create(GameRegisterDto gameRegister)
+        {
+            var game = await Create(gameRegister);
+            return _mapper.Map<GameRegisterDto>(game);
+        }
+
+        public async Task<IEnumerable<GameResponseDto>> GetAll()
 		{
-			return [.. await Get()];
-		}
+			var games =  await Get();
+            return [.. games.Select(g => _mapper.Map<GameResponseDto>(games))];
+        }
+
+        public async Task<GameResponseDto?> GetById(int id)
+        {
+            var game = await Get(id);
+            return game is null ? null : _mapper.Map<GameResponseDto>(game);
+        }
+
+        public async Task<bool> Update(int id, GameUpdateDto gameUpdateDto)
+        {
+            var game = await Get(id) ?? throw new ArgumentNullException(nameof(id), $"Erro ao atualizar: Jogo inexistente!");
+            _mapper.Map(gameUpdateDto, game);
+            return await Edit(game);
+        }
+
+        public async Task<bool> Remove(int id)
+        {
+            return await Delete(id);
+        }
     }
 }
--- FIAP.FCG.Infra/Repository/IGameRepository.cs
+++ FIAP.FCG.Infra/Repository/IGameRepository.cs
@@ -1,9 +1,15 @@
-﻿using FIAP.FCG.Core.Models;
+﻿using FIAP.FCG.Core.Inputs;
+using FIAP.FCG.Core.Models;
 
 namespace FIAP.FCG.Infra.Repository
 {
 	public interface IGameRepository
 	{
-        Task<IEnumerable<Game>> GetAll();
-	}
+		Task<GameRegisterDto> Create(GameRegisterDto gameRegister);
+        Task<IEnumerable<GameResponseDto>> GetAll();
+		Task<GameResponseDto> GetById(int id);
+		Task<bool> Update(int id, GameUpdateDto gameUpdateDto);
+		Task<bool> Remove(int id);
+
+    }
 }
